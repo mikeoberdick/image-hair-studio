ef2a8485d61d85aa1e4195d1c867e7b0bcd47395
Mike Oberdick <mikeoberdick@gmail.com>
2017-11-10 06:20:31 -0500

    Services Page Navigation & Content

--- template-parts/content-services.php
+++ template-parts/content-services.php
@@ -1,116 +1,160 @@
 <div id = "servicesPage">
 	<div class="container">
-			<div class="row">
-				<div class="col-sm-12 col-md-4">
-					<h5>Haircutting and Styling</h5>
-					<div class = "row">
-						<div class="col-sm-6">
-							Haircut & Style</br>
-							Dry Haircut</br>
-							Mens Haircut</br>
-							Children's Haircut
+		<div class="row">
+			<div class="col-md-3">
+				<h5>Services Menu</h5>
+				<ul id = "servicesMenu" class="nav nav-pills flex-column" role="tablist">
+				  <li class="nav-item">
+				   <a class="nav-link active" data-toggle="tab" href="#haircutting_and_styling" role="tab">Haircutting & Styling</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" data-toggle="tab" href="#blowdry" role="tab">Blowdry</a>
+				  </li>
+				  <li class="nav-item">
+				   <a class="nav-link" data-toggle="tab" href="#color_services" role="tab">Color Services</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" data-toggle="tab" href="#treatments" role="tab">Treatments</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" data-toggle="tab" href="#texturizing_waves" role="tab">Texturizing Waves</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" data-toggle="tab" href="#air_brush_makeup" role="tab">Air Brush Makeup</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" data-toggle="tab" href="#waxing" role="tab">Waxing</a>
+				  </li>
+				</ul>
+			</div><!-- .col-md-4 -->
+
+			<div class="col-md-9">
+				<div class="tab-content">
+				  <div class="tab-pane active" id="haircutting_and_styling" role="tabpanel">
+				  	<h3>Haircutting and Styling</h3>
+				  	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque labore natus quod nulla est. Tenetur impedit, velit sit. Reprehenderit maiores provident aliquid praesentium maxime. Dolorem qui aut iste porro mollitia?</p>
+						<div class = "row">
+							<div class="col-sm-4">
+								Haircut & Style</br>
+								Dry Haircut</br>
+								Mens Haircut</br>
+								Children's Haircut
+							</div>
+							<div class="col-sm-4">
+								50</br>
+								32</br>
+								25</br>
+								20
+							</div>
 						</div>
-						<div class="col-sm-6">
-							50</br>
-							32</br>
-							25</br>
-							20
+				  </div><!-- .tab-pane -->
+
+	 				<div class="tab-pane" id="blowdry" role="tabpanel">
+					  <h3>Blowdry</h3>
+						<div class = "row">
+							<div class="col-sm-4">
+								Blowdry</br>
+								Up Styling</br>
+								Iron Work
+							</div>
+							<div class="col-sm-4">
+								30</br>
+								75</br>
+								10
+							</div>
 						</div>
-					</div>
-				</div>
-				<div class="col-sm-12 col-md-4">
-					<h5>Blowdry</h5>
-					<div class = "row">
-						<div class="col-sm-6">
-							Blowdry</br>
-							Up Styling</br>
-							Iron Work
+					</div><!-- .tab-pane -->
+				  
+				  <div class="tab-pane" id="color_services" role="tabpanel">
+				  	<h3>Color Services</h3>
+				  		<div class = "row">
+							<div class="col-sm-4">
+								Root Touch Up</br>
+								Full Color</br>
+								Full Foil Highlight</br>
+								Partial Foil Highlight</br>
+								Gloss</br>
+								10 Pack Foil</br>
+								Belayge<br>
+								Ombre:</br>
+								Men's Color</br>
+								*Long Hair Extra
+							</div>
+							<div class="col-sm-4">
+								50</br>
+								32</br>
+								25</br>
+								20
+							</div>
 						</div>
-						<div class="col-sm-6">
-							30</br>
-							75</br>
-							10
+				  </div>
+
+				 <div class="tab-pane" id="treatments" role="tabpanel">
+					  <h3>Treatments</h3>
+						<div class = "row">
+							<div class="col-sm-4">
+								Blowdry</br>
+								Up Styling</br>
+								Iron Work
+							</div>
+							<div class="col-sm-4">
+								30</br>
+								75</br>
+								10
+							</div>
 						</div>
-					</div>
-				</div>
-				<div class="col-sm-12 col-md-4">
-					<h5>Color Services</h5>
-					<div class = "row">
-						<div class="col-sm-6">
-							Root Touch Up</br>
-							Full Color</br>
-							Full Foil Highlight</br>
-							Partial Foil Highlight</br>
-							Gloss</br>
-							10 Pack Foil</br>
-							Belayge<br>
-							Ombre:</br>
-							Men's Color</br>
-							*Long Hair Extra
+					</div><!-- .tab-pane -->
+
+					<div class="tab-pane" id="texturizing_waves" role="tabpanel">
+					  <h3>Texturizing Waves</h3>
+						<div class = "row">
+							<div class="col-sm-4">
+								Blowdry</br>
+								Up Styling</br>
+								Iron Work
+							</div>
+							<div class="col-sm-4">
+								30</br>
+								2</br>
+								10
+							</div>
 						</div>
-						<div class="col-sm-6">
-							50</br>
-							32</br>
-							25</br>
-							20
+					</div><!-- .tab-pane -->
+
+					<div class="tab-pane" id="air_brush_makeup" role="tabpanel">
+					  <h3>Air Brush Makeup</h3>
+						<div class = "row">
+							<div class="col-sm-4">
+								Blowdry</br>
+								Up Styling</br>
+								Iron Work
+							</div>
+							<div class="col-sm-4">
+								30</br>
+								3</br>
+								10
+							</div>
 						</div>
-					</div>
-				</div>
-			</div>
-			
-						<div class="row">
-				<div class="col-sm-12 col-md-4">
-					<h5>Treatments</h5>
-					<div class = "row">
-						<div class="col-sm-6">
-							Haircut & Style</br>
-							Dry Haircut</br>
-							Mens Haircut</br>
-							Children's Haircut
+					</div><!-- .tab-pane -->
+
+					<div class="tab-pane" id="waxing" role="tabpanel">
+					  <h3>Waxing</h3>
+						<div class = "row">
+							<div class="col-sm-4">
+								Blowdry</br>
+								Up Styling</br>
+								Iron Work
+							</div>
+							<div class="col-sm-4">
+								30</br>
+								3</br>
+								10
+							</div>
 						</div>
-						<div class="col-sm-6">
-							50</br>
-							32</br>
-							25</br>
-							20
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-12 col-md-4">
-					<h5>Texturizing Waves</h5>
-					<div class = "row">
-						<div class="col-sm-6">
-							Haircut & Style</br>
-							Dry Haircut</br>
-							Mens Haircut</br>
-							Children's Haircut
-						</div>
-						<div class="col-sm-6">
-							50</br>
-							32</br>
-							25</br>
-							20
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-12 col-md-4">
-					<h5>Additional Servies</h5>
-					<div class = "row">
-						<div class="col-sm-6">
-							Haircut & Style</br>
-							Dry Haircut</br>
-							Mens Haircut</br>
-							Children's Haircut
-						</div>
-						<div class="col-sm-6">
-							50</br>
-							32</br>
-							25</br>
-							20
-						</div>
-					</div>
-				</div>
-			</div>
+					</div><!-- .tab-pane -->
+
+				</div><!-- .tab-content -->
+			</div><!-- .col-md-8 -->
 		</div>
 	</div><!-- .container -->
 </div><!-- #servicesPage -->
